00fc58c20995f64a3f38575d8938922081f34cbc
Rafael Martins <33657518+rsmartins78@users.noreply.github.com>
2019-03-17 13:35:41 -0300

    added 2 lines

--- actions/send_message.py
+++ actions/send_message.py
@@ -3,6 +3,7 @@ from emoji import emojize
 
 from st2common.runners.base_action import Action
 
+
 class TelegramSendMessageAction(Action):
     def run(self, message, chat_id):
         bot = telegram.Bot(token=self.config['apikey'])
