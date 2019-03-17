94e770eee15c94b580eba263c37278ab9971380e
Rafael Martins <33657518+rsmartins78@users.noreply.github.com>
2019-03-17 13:24:42 -0300

    Now is sendind emojis.

--- actions/send_message.py
+++ actions/send_message.py
@@ -1,10 +1,10 @@
 import telegram
+from emoji import emojize
 
 from st2common.runners.base_action import Action
-
 
 class TelegramSendMessageAction(Action):
     def run(self, message, chat_id):
         bot = telegram.Bot(token=self.config['apikey'])
-        m = bot.sendMessage(text=message, chat_id=chat_id)
+        m = bot.sendMessage(text=emojize(message, use_aliases=True), chat_id=chat_id)
         return m
